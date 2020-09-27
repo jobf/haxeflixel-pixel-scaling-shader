477dd6b9fa9d629b9824dbc4fec490b841e7da75
jf <jf>
2020-09-27 03:38:52 +0100

    draw test squares to screen

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.util.FlxColor;
 import openfl.filters.ShaderFilter;
 
 class PlayState extends FlxState
@@ -12,6 +13,18 @@ class PlayState extends FlxState
 		super.create();
 		FlxG.camera.setFilters([new ShaderFilter(new PixelScaleShader())]);
 		add(new FlxSprite(0, 0, "assets/images/pixel-grid.png"));
+
+		var sqSize = 4;
+		var colors = [FlxColor.RED, FlxColor.GREEN, FlxColor.BLUE, FlxColor.WHITE];
+		for (y in 0...1)
+		{
+			for (x in 0...4)
+			{
+				var pixel = new FlxSprite(x * sqSize, y);
+				pixel.makeGraphic(sqSize, sqSize, colors[x]);
+				add(pixel);
+			}
+		}
 	}
 
 	override public function update(elapsed:Float)
